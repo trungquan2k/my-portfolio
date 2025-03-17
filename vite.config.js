c678c2ce48f29321dc501c24d96c45298dae63a8
quanht <144759613+quanhtsparkminds@users.noreply.github.com>
2025-03-17 23:12:16 +0700

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 /* eslint-disable no-undef */
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+    mimeTypes: {
+      jsx: 'text/javascript',
+    },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
