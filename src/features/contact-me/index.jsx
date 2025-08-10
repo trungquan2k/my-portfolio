b43bb7752d1125340b77c4b754ad9d95e80ffae2
quanht <quanht@sparkminds.net>
2025-08-10 19:28:56 +0700

    chore: update cv

--- src/features/contact-me/index.jsx
+++ src/features/contact-me/index.jsx
@@ -28,7 +28,7 @@ const ContactMeView = () => {
                 </Paragraph>
                 <div className="flex items-center mb-4">
                   <FaPhone className="text-2xl text-blue-600 mr-4" />
-                  <p className="text-white text-justify">(+84) 877 289 816</p>
+                  <p className="text-white text-justify">(+84) 355 739 816</p>
                 </div>
               </div>
               <div>
